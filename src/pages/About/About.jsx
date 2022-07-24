4d844990524b4f3ad18472953b8457bf93b267c5
Stefan Pusica <pusicastefan1@gmail.com>
2022-07-24 23:37:32 +0200

    Update1

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -12,7 +12,7 @@ function About() {
               <div className="about-row-left">
                 <p className='about-row-left-text'>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
-                    standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make 
+                    standard dummy text ever since thee 1500s, when an unknown printer took a galley of type and scrambled it to make 
                     a type specimen book. 
                 </p>
               </div>
